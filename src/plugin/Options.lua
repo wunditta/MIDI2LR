244b9480ea4425bcce5386e4169bc9838503dcea
wunditta <50488451+wunditta@users.noreply.github.com>
2023-09-23 16:53:55 +0200

    Add Copy Presets tab, Moved other settings to separate tab

--- src/plugin/Options.lua
+++ src/plugin/Options.lua
@@ -26,6 +26,9 @@ local LocalPresets      = require 'LocalPresets'
 local OU                = require 'OptionsUtilities'
 local Preferences       = require 'Preferences'
 local Profiles          = require 'Profiles'
+local Presets           = require 'Presets'
+local CopySettings      = require 'CopySettings'
+local Init              = require 'Init'
 local LrBinding         = import 'LrBinding'
 local LrDialogs         = import 'LrDialogs'
 local LrFunctionContext = import 'LrFunctionContext'
@@ -41,10 +44,18 @@ local function setOptions()
       --]]-----------end debug section
       local f = LrView.osFactory()
       local properties = LrBinding.makePropertyTable( context )
+      local OptLimits = Init.OptionLimits
       --following not managed by another module
+      -- See Init.lua for default values!!!
       properties.ClientShowBezelOnChange = ProgramPreferences.ClientShowBezelOnChange
       properties.TrackingDelay = ProgramPreferences.TrackingDelay
       properties.RevealAdjustedControls = ProgramPreferences.RevealAdjustedControls
+      --properties.DeleteAnyhow = ProgramPreferences.DeleteAnyhow
+      properties.FineEnabled = ProgramPreferences.FineEnabled
+      properties.RememberSelection = ProgramPreferences.RememberSelection
+      properties.FineMagnify = ProgramPreferences.FineMagnify
+      properties.FineAutoSensitivity = ProgramPreferences.FineAutoSensitivity
+      properties.FineDeactivate = ProgramPreferences.FineDeactivate
 
       -- assemble dialog box contents
       local contents =
@@ -62,17 +73,8 @@ local function setOptions()
             Filters.StartDialog(properties,f),
           },
           f:tab_view_item {
-            title = LOC("$$$/AgPrint/ProfileMenu/Other=Other..."),
-            identifier = 'othersettings',
-            f:view(Limits.StartDialog(properties,f)),
-            f:separator {fill_horizontal = 0.9},
-            f:row {
-              f:checkbox {title = LOC("$$$/AgDocument/ModulePicker/Settings/ShowStatusAndActivity=Show status and activity"), value = LrView.bind('ClientShowBezelOnChange')},
-              f:checkbox {title = LOC("$$$/MIDI2LR/Options/RevealAdjustedControls=Reveal adjusted controls"), value = LrView.bind('RevealAdjustedControls')},
-              f:spacer {width = 40},
-              OU.slider(f,properties,LOC("$$$/MIDI2LR/Options/TrackingDelay=Tracking Delay"),'slidersets','TrackingDelay',0,3,2),
-            }, -- row
-            f:separator {fill_horizontal = 0.9},
+            title = LOC("$$$/MIDI2LR/Options/ShortcutsMisc=Keyboard shortcuts"),
+            identifier = 'keys',
             Keys.StartDialog(properties,f),
           }, -- tab_view_item
           f:tab_view_item {
@@ -85,6 +87,42 @@ local function setOptions()
             identifier = 'localpresets',
             LocalPresets.StartDialog(properties,f),
           }, --tab_view_item
+          f:tab_view_item {
+            title = LOC("$$$/MIDI2LR/CopyPresets/CopyPresets=Copy Develop Setting Presets"),
+            identifier = 'copypresets',
+            CopySettings.StartDialog(properties,f),
+          }, --tab_view_item
+          f:tab_view_item {
+            title = LOC("$$$/LibraryMetadata/IPTCEx/Others=Others"),
+            identifier = 'othersettings',
+            f:column {
+              Limits.StartDialog(properties,f),
+              --f:separator {fill_horizontal = 0.9},
+              f:spacer { height = 10},
+              f:group_box {
+                title = LOC("$$$/LibraryMetadata/IPTCEx/Others=Others"),
+                f:row {
+                  f:checkbox {title = LOC("$$$/AgDocument/ModulePicker/Settings/ShowStatusAndActivity=Show status and activity"), value = LrView.bind('ClientShowBezelOnChange')},
+                  f:checkbox {title = LOC("$$$/MIDI2LR/Options/RevealAdjustedControls=Reveal adjusted controls"), value = LrView.bind('RevealAdjustedControls')},
+                  f:spacer {width = 20},
+                  OU.slider(f,properties,LOC("$$$/MIDI2LR/Options/TrackingDelay=Tracking Delay"),'slidersets','TrackingDelay',0,3,2),
+                  --f:checkbox {title = LOC("$$$/AgLibrary/Trash/NoTrash/DeleteAnyhow=Delete Files permanently"), value = LrView.bind('DeleteAnyhow')},
+                }, -- row
+                f:row {
+                  f:checkbox {title = LOC("$$$/MIDI2LR/Options/RememberSel=Remember Selection for each folder"), value = LrView.bind('RememberSelection')},
+                }, -- row
+              --f:separator {fill_horizontal = 0.9},
+              }, -- group_box
+              f:spacer { height = 10},
+              f:group_box {
+                title = LOC("$$$/MIDI2LR/Finetune/Title=Fine-tuning"),
+                f:checkbox {title = LOC("$$$/MIDI2LR/Finetune/Enable=Enable Fine-tuning"), value = LrView.bind('FineEnabled')},
+                OU.slider(f,properties,LOC("$$$/MIDI2LR/Finetune/Magnify=Movement Magnification"),'','FineMagnify',1,10,OptLimits.FineMagnify),
+                OU.slider(f,properties,LOC("$$$/MIDI2LR/Finetune/AutoSensitivity=Auto-activate Sensitivity (0=disable)"),'','FineAutoSensitivity',0,20,OptLimits.FineAutoSensitivity),
+                OU.slider(f,properties,LOC("$$$/MIDI2LR/Finetune/AutoTimeout=Deactivation Timeout (0=disable)"),'','FineDeactivate',0,10,OptLimits.FineDeactivate,true),
+              }, -- group_box
+            }, -- column
+          }, -- tab_view_item
         }, -- tab_view
       } -- view
 
@@ -99,6 +137,8 @@ local function setOptions()
       Limits.EndDialog(properties,result)
       LocalPresets.EndDialog(properties,result)
       Profiles.EndDialog(properties,result)
+      Presets.EndDialogGroups(properties,result)
+      CopySettings.EndDialog(properties,result)
       if result == 'ok' then
         local LrDevelopController = import 'LrDevelopController'
         --following not managed by another lua module file
@@ -112,6 +152,12 @@ local function setOptions()
           ProgramPreferences.RevealAdjustedControls = properties.RevealAdjustedControls
           LrDevelopController.revealAdjustedControls(ProgramPreferences.RevealAdjustedControls)
         end
+        --ProgramPreferences.DeleteAnyhow = properties.DeleteAnyhow
+        ProgramPreferences.RememberSelection = properties.RememberSelection
+        ProgramPreferences.FineEnabled = properties.FineEnabled
+        ProgramPreferences.FineMagnify = properties.FineMagnify
+        ProgramPreferences.FineAutoSensitivity = properties.FineAutoSensitivity
+        ProgramPreferences.FineDeactivate = properties.FineDeactivate
 
         --then save preferences
         Preferences.Save()
